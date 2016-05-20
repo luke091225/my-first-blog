5e789902bd15591e974ecab28639aec6686854f6
luke01 <oedfish@gmail.com>
2016-05-20 06:20:28 +0000

    complete tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME' : 'djangogirls',
+#        'USER' : 'djangogirls',
+#        'PASSWORD' : 'djangogirls@123',
+#        'HOST' : 'localhost',
+#        'PORT' : '5432',
+#        }
 }
 
 
@@ -102,3 +110,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout$','django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'', include('blog.urls')),
 ]
